92aab4dac6348a6c0a0647e9437ac5c4cf0a9519
Hannah Pullen <hannah.pullen@physics.ox.ac.uk>
2018-03-12 11:14:55 +0000

    Fixed plotting of significance fit + added stats box

--- Fit_data_new/Macros/PlotSignificance.C
+++ Fit_data_new/Macros/PlotSignificance.C
@@ -1,30 +1,36 @@
 // Script for fitting and plotting a significance distribution
-void PlotSignificance() {
+void PlotSignificance(TString mode) {
 
     // =====
     // Setup
     // =====
     // Open the files
     TChain * tree = new TChain("toy_tree");
-    tree->Add("../Results/Significance/significance_piK_*.root");
+    tree->Add("../Results/Significance/significance_" + mode + "_*.root");
     std::cout << "Loaded tree with " << tree->GetEntries() << " entries."
         << std::endl;
     std::cout << "Entries with status = 0: " << tree->GetEntries("status == 0");
 
     // Make and fill histogram
-    TH1F * hist = new TH1F("hist_sig", "", 100, 0, 10);
+    TH1F * hist = new TH1F("Significance", "#piK significance", 100, 0, 10);
     hist->SetStats(true);
-    tree->Draw("significance>>hist_sig", "status == 0");
+    tree->Draw("significance>>Significance", "status == 0");
 
-    // // Fit
-    // hist->Fit("gaus");
-    // TF1 * gauss_fit = hist->GetFunction("gaus");
-    // gauss_fit->SetLineColor(862);
-    // gauss_fit->SetLineWidth(1);
+    // Fit
+    hist->Fit("gaus");
+    TF1 * gauss_fit = hist->GetFunction("gaus");
+    gauss_fit->SetLineColor(862);
+    gauss_fit->SetLineWidth(1);
 
     // Draw
     TCanvas * canvas = new TCanvas("canvas", "", 500, 400);
-    // gauss_fit->Draw("C SAME");
-    hist->Draw("E SAME");
-    canvas->SaveAs("../Plots/Significance/significance.pdf");
+    hist->SetLineWidth(1);
+    hist->GetXaxis()->SetTitle("Significance");
+    hist->GetYaxis()->SetTitle("Number of toys");
+    gStyle->SetOptStat(true);
+    gStyle->SetStatX(0.85);
+    hist->Draw("E");
+    gauss_fit->SetLineWidth(2);
+    gauss_fit->Draw("C SAME");
+    canvas->SaveAs("../Plots/Significance/significance_" + mode + ".pdf");
 }
