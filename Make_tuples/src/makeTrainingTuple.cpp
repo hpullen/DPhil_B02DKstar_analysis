5e6500495babd6be20eeb2790a3a07fb359b1600
Hannah Pullen <hannah.pullen@physics.ox.ac.uk>
2018-04-10 15:37:11 +0100

    Updated MakeTrainingTuple script to use new CutReader

--- Make_tuples/src/makeTrainingTuple.cpp
+++ Make_tuples/src/makeTrainingTuple.cpp
@@ -4,28 +4,17 @@
 
 #include "TChain.h"
 #include "TFile.h"
-#include "TLorentzVector.h"
+#include "TEventList.h"
 #include "TTree.h"
 
-inline double Product(const TLorentzVector& p1, const TLorentzVector& p2,
-        const TLorentzVector& pX) {
-    return - p1.Dot(p2) + p1.Dot(pX) * p2.Dot(pX) / pX.Dot(pX);
-}
-
-void LorentzVectorBranch(TTree* tree, TLorentzVector& vec,
-        const std::string& name) {
-    tree->SetBranchAddress((name + "_PE").c_str(), &vec[3]);
-    tree->SetBranchAddress((name + "_PX").c_str(), &vec[0]);
-    tree->SetBranchAddress((name + "_PY").c_str(), &vec[1]);
-    tree->SetBranchAddress((name + "_PZ").c_str(), &vec[2]);
-}
+#include "CutReader.hpp"
 
 int main(int argc, char * argv[]) {
 
     // Check for correct number of args
     if (argc != 6 && argc != 7) {
         std::cout << "Usage: ./MakeTrainingTuple <data/MC> <2011/2012/2015/2016> "
-            "<up/down> <twoBody/fourBody> <D0mode> (<treename>)" << std::endl;
+            "<up/down> <twoBody/fourBody> <D0mode>" << std::endl;
         return -1;
     }
 
@@ -44,13 +33,6 @@ int main(int argc, char * argv[]) {
     // D0 decay mode
     std::string mode = argv[5];
 
-    // Optional: treename
-    std::string treename = "Tuple_" + mode;
-    if (argc == 7) {
-        treename = argv[6];
-    }
-    treename += "/DecayTree";
-
     // Path to input file
     std::string inputPath = "/data/lhcb/users/pullen/B02DKstar/" + type + "/" +
         bodies + "/";
@@ -58,13 +40,12 @@ int main(int argc, char * argv[]) {
         inputPath += mode + "/";
     }
     inputPath += year + "_" + mag + "/";
-    std::string inputFile = inputPath + "Total.root";
-    std::cout << "Taking input from tree " << treename << " in file " <<
-        inputFile << std::endl;
+    std::string inputFile = inputPath + mode + ".root";
+    std::cout << "Taking input from file " << inputFile << std::endl;
 
     // Read input file and tree
     TFile * file = TFile::Open(inputFile.c_str(), "READ");
-    TTree * tree = (TTree*)file->Get(treename.c_str());
+    TTree * tree = (TTree*)file->Get("DecayTree");
 
     // Make output file and tree
     std::string outputFile = inputPath + mode + "_trainingSample.root";
@@ -72,80 +53,36 @@ int main(int argc, char * argv[]) {
     TFile * new_file = TFile::Open(outputFile.c_str(), "RECREATE");
     TTree * new_tree = (TTree*)tree->CloneTree(0);
 
-    // Variables needed
-    double Kstar_M;
-    double D0_M;
-    float Bd_ConsD_M;
-    double helicityAngle;
-    int bkgCat = 0;
-    TLorentzVector KstarK_P, KstarPi_P, D0_P;
-
-    // Constants
-    const double D0_PDG_M = 1864.84;
-    const double D0_range = 25.0;
-    const double Kstar_PDG_M = 895.55;
-    const double Kstar_range = 50.0;
-
-    // Set branch addresses
-    tree->SetBranchAddress("Kstar_M", &Kstar_M);
-    tree->SetBranchAddress("D0_M", &D0_M);
-    tree->SetBranchAddress("Bd_ConsD_M", &Bd_ConsD_M);
-    if (type == "MC") {
-        tree->SetBranchAddress("Bd_BKGCAT", &bkgCat);
+    // Get cut
+    CutReader * cr = new CutReader();
+    TCut cut = cr->GetCut("D0_M ");
+    if (type == "data") {
+        cut += "Bd_ConsD_M > 5800";
+    } else {
+        cut += "Bd_BKGCAT == 0";
     }
 
-    // Make TLorentzVector branches
-    LorentzVectorBranch(tree, KstarK_P, "KstarK");
-    LorentzVectorBranch(tree, KstarPi_P, "KstarPi");
-    LorentzVectorBranch(tree, D0_P, "D0");
+    // Draw to EventList
+    TEventList * elist = new TEventList("elist", "");
+    tree->Draw(">>elist", cut);
 
-    // Make new branch for helicity angle
-    new_tree->Branch("Kstar_helicity_angle", &helicityAngle, 
-            "Kstar_helicity_angle/D");
-
-    // Loop over tree entries
-    int n_entries = tree->GetEntries();
-    std::cout << "Looping over " << n_entries << " entries." << std::endl;
-    int n_pass = 0; 
-    for (int i = 0; i < n_entries; i++) {
-        tree->GetEntry(i);
-        if (i % 100000 == 0) {
-            std::cout << "Processing event " << i << std::endl;
-        }
-
-        // Work out helicity angle
-        helicityAngle = Product(KstarK_P, D0_P, KstarK_P + KstarPi_P) / 
-            sqrt(Product(KstarK_P, KstarK_P, KstarK_P + KstarPi_P) * 
-                    Product(D0_P, D0_P, KstarK_P + KstarPi_P));
-
-        // Fill tree if cuts are passed
-        if (type == "MC") {
-            if (std::abs(Kstar_M - Kstar_PDG_M) < Kstar_range 
-                    && std::abs(D0_M - D0_PDG_M) < D0_range
-                    && std::abs(helicityAngle) > 0.4
-                    && bkgCat == 0) {
-                new_tree->Fill();
-                n_pass++;
-            }
-        } else if (type == "data") {
-            if (std::abs(Kstar_M - Kstar_PDG_M) < Kstar_range 
-                    && std::abs(D0_M - D0_PDG_M) < D0_range
-                    && std::abs(helicityAngle) > 0.4
-                    && Bd_ConsD_M > 5800) {
-                new_tree->Fill();
-                n_pass++;
-            }
-        }
+    // Fill new tree
+    int passed = 0;
+    for (int i = 0; i < elist->GetSize(); i++) {
+        int evt = elist->GetEntry(i);
+        tree->GetEntry(evt);
+        new_tree->Fill();
+        passed++;
     }
+    std::cout << "Filled new tree with " << passed << " entries.\n" << std::endl;
 
-    std::cout << "Filled new tree with " << n_pass << " entries.\n" << std::endl;
-    
     // Save
     new_tree->Write();
     new_file->Close();
     delete tree;
     file->Close();
     delete file;
+    delete cr;
 
     return 0;
 }
