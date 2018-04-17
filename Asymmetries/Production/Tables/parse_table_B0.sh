ebbf7b30c9aa08ebde4e883b900dfd9785987635
Hannah Pullen <hannah.pullen@physics.ox.ac.uk>
2018-04-17 15:27:31 +0100

    Parsed B0 vs Bs production asymmetry tables

---
+++ Asymmetries/Production/Tables/parse_table_B0.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+FILE=$1
+OUTFILE=$(basename $(echo $FILE | sed 's/_raw//') .tex).txt
+cat $FILE | grep '^\$' | sed 's/\$(//g; s/)\$//g; s/,//g; s/\\phantom{-}//g'\
+    | awk '{print $1, $2, $4, $5, $15, $17, $19}' > $OUTFILE
