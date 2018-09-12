85b52da1a6ffb24f82516539d79bcb4179e079ff
Hannah Pullen <hannah.pullen@physics.ox.ac.uk>
2018-09-12 17:28:34 +0100

    Added normal notation function to s_no"

--- ANA_scripts/Tables/s_no.sh
+++ ANA_scripts/Tables/s_no.sh
@@ -2,7 +2,7 @@
 
 # Get power of 10 of number
 get_exponent() {
-    
+
     # Value and log
     VALUE=$1
 
@@ -96,4 +96,87 @@ s_no() {
 
 }
 
-# s_no $*
+
+# Normal (not scientific) notation
+n_no() {
+
+    # Get value and error
+    VALUE=$1
+    ERROR=$2
+
+    # Get sign of value
+    if [[ $(bc -l <<< "$VALUE < 0") == 1 ]]; then
+        SIGN="-"
+        VALUE=$(echo $VALUE | sed "s/-//")
+    else
+        SIGN=""
+    fi
+
+    # Check for zero value
+    if [[ $(bc -l <<< "$VALUE == 0") == 1 ]]; then
+
+        EXP=$(($(get_exponent $ERROR) + 1))
+        PREC=1
+
+    else 
+
+        # Work out precision
+        N_DP_VALUE=$(get_exponent $VALUE)
+        N_DP_ERROR=$(get_exponent $ERROR)
+        if [[ $(bc -l <<< "$N_DP_ERROR > $N_DP_VALUE") == 1 ]]; then
+            EXP_VAL=$ERROR
+            PREC=1
+        else
+            EXP_VAL=$VALUE
+            PREC=$((N_DP_VALUE - N_DP_ERROR + 1))
+        fi
+
+        # Find exponent
+        EXP=$(($(get_exponent $EXP_VAL) + 1))
+    fi
+
+    # Correct precision if first digit is one or two
+    FIRST_ERROR_DIGIT=$(echo $ERROR | sed 's/[\.0]*\([^\.0]\).*/\1/')
+    KEEP_ERR=1
+    if [[ $FIRST_ERROR_DIGIT < 3 ]]; then
+        PREC=$((PREC + 1))
+        KEEP_ERR=2
+    fi
+
+    # Case where full number is printed
+    if [[ $(bc -l <<< "$EXP >= 1") == 1 ]] && [[ $(bc -l <<< "$PREC == $EXP") == 1 ]]; then
+        VALUE_FINAL=$(printf "%.0f" $VALUE)
+        ERROR_FINAL=$(printf "%.0f" $ERROR)
+
+    # Case where number is truncated
+    elif [[ $(bc -l <<< "$EXP >= 1") == 1 ]] && [[ $(bc -l <<< "$PREC < $EXP") == 1 ]]; then
+
+        # Significant digits
+        VALUE_FINAL=$(printf "%.${PREC}g" $VALUE | sed 's/\([1-9]\)\.*\([0-9]*\).*/\1\2/')
+        ERROR_FINAL=$(printf "%.${KEEP_ERR}g" $ERROR | sed 's/\([1-9]\)\.*\([0-9]*\).*/\1\2/')
+
+        # Add extra zeros
+        ZEROS=$(($EXP - ${#VALUE_FINAL}))
+        ZEROS_ERR=$(($N_DP_ERROR + 1 - ${#ERROR_FINAL}))
+        COUNT=0
+        while [[ $COUNT -lt $ZEROS ]]; do
+            VALUE_FINAL="${VALUE_FINAL}0"
+            COUNT=$((COUNT + 1))
+        done
+        COUNT=0
+        while [[ $COUNT -lt $ZEROS_ERR ]]; do
+            ERROR_FINAL="${ERROR_FINAL}0"
+            COUNT=$((COUNT + 1))
+        done
+
+    # Case with decimal places
+    else 
+        N_DP=$((PREC - EXP))
+        VALUE_FINAL=$(printf "%.${N_DP}f" $VALUE)
+        ERROR_FINAL=$(printf "%.${N_DP}f" $ERROR)
+    fi
+
+    # Print
+    echo "\$${SIGN}${VALUE_FINAL} \\pm ${ERROR_FINAL}\$"
+
+}
