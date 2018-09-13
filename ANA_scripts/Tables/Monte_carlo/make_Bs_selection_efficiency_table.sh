5d18bf15bffccbaf7f6a39809f95164d41641970
Hannah Pullen <hannah.pullen@physics.ox.ac.uk>
2018-09-13 23:20:11 +0100

    Made Bs table

--- ANA_scripts/Tables/Monte_carlo/make_Bs_selection_efficiency_table.sh
+++ ANA_scripts/Tables/Monte_carlo/make_Bs_selection_efficiency_table.sh
@@ -38,7 +38,7 @@ print_line 1
 print_line 2
 echo '        \bottomrule'                                       >> $TAB_FILE
 echo '    \end{tabular}'                                         >> $TAB_FILE
-echo '    \\caption{Acceptance and selection efficiencies from '\
+echo '    \caption{Acceptance and selection efficiencies from '\
          '$B_s \to DK^{*0}$ Monte Carlo. Total efficiency is the '\
          'product of these.}'                                    >> $TAB_FILE
 echo "\\label{tab:selection_efficiency_Bs}"                    >> $TAB_FILE
