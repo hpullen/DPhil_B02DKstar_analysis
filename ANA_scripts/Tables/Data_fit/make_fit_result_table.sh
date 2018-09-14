f471d5235368bda8431353a247330306d8068e95
Hannah Pullen <hannah.pullen@physics.ox.ac.uk>
2018-09-14 22:44:26 +0100

    Split fit result table into 2 columns

--- ANA_scripts/Tables/Data_fit/make_fit_result_table.sh
+++ ANA_scripts/Tables/Data_fit/make_fit_result_table.sh
@@ -1,4 +1,5 @@
 # Make table from fit result parameters
+#!/usr/bin/env bash  
 source ../s_no.sh
 
 # Print one line
@@ -28,17 +29,42 @@ OUTFILE="../../../ANA_resources/Tables/Data_fit/fit_result.tex"
 echo '\begin{table}' > $OUTFILE
 echo '  \centering' >> $OUTFILE
 echo '  \begin{tabular}{cc}' >> $OUTFILE
+
+# Start left column table
+echo '      \begin{tabular}{cc}' >> $OUTFILE
 echo '      \toprule' >> $OUTFILE
 echo '      Parameter & Value \\' >> $OUTFILE
 echo '      \midrule' >> $OUTFILE
 
+# Get number of parameters
+N_PARS=$(cat raw_fit_result.param | wc -l)
+
 # Print all parameters
+COUNT=1
+SWITCHED=false
 for PAR in $(awk '{print $1}' raw_fit_result.param); do
+
+    # Check if halfway; start new table column
+    if [[ $SWITCHED == "false" && $(bc -l <<< "$COUNT > $N_PARS/2") == 1 ]]; then
+        echo "Count: ${COUNT}. Switching to new column."
+        echo '      \bottomrule' >> $OUTFILE
+        echo '      \end{tabular}' >> $OUTFILE
+        echo '      & \begin{tabular}{cc}' >> $OUTFILE
+        echo '      \toprule' >> $OUTFILE
+        echo '      Parameter & Value \\' >> $OUTFILE
+        echo '      \midrule' >> $OUTFILE
+        SWITCHED=true
+    fi
+
+    # Print line
     print_line $PAR >> $OUTFILE
+    COUNT=$((COUNT + 1))
+
 done
 
 # Finish table
 echo '      \bottomrule' >> $OUTFILE
+echo '      \end{tabular} \\' >> $OUTFILE
 echo '  \end{tabular}' >> $OUTFILE
 echo '  \caption{Values obtained for floating parameters in the fit to the invariant mass of selected data candidates.}' >> $OUTFILE
 echo '\label{tab:fit_result}' >> $OUTFILE
