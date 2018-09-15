a115bc7685594c853e68a92696aa7f964ee5fce0
Hannah Pullen <hannah.pullen@physics.ox.ac.uk>
2018-09-15 16:18:16 +0100

    Added combined fit plots to snakemake

--- ANA_scripts/Plots/Data_fit/get_data_plots.sh
+++ ANA_scripts/Plots/Data_fit/get_data_plots.sh
@@ -1,14 +1,25 @@
 #!/usr/bin/env bash 
 FIT_DIR=$HOME/analysis/B02DKstar/Fit_data_new/Plots
 PLOT_DIR=$HOME/analysis/B02DKstar/ANA_resources/Plots/Data_fit
+if [[ $1 == "--combined" ]]; then
+    COMB=true
+else 
+    COMB=false
+fi
 
 for mode in Kpi piK KK pipi Kpipipi piKpipi pipipipi; do
-    for sign in plus minus; do
-        for run_num in 1 2; do
+    for run_num in 1 2; do
+        if [[ $COMB == true ]]; then
             if [[ ! ($run_num == "1" && $mode == "pipipipi") ]]; then
-                cp $FIT_DIR/twoAndFourBody_data_split_${mode}_run${run_num}_${sign}.pdf $PLOT_DIR
+                cp $FIT_DIR/twoAndFourBody_data_${mode}_run${run_num}.pdf $PLOT_DIR
             fi
-        done
+        else 
+            for sign in plus minus; do
+                if [[ ! ($run_num == "1" && $mode == "pipipipi") ]]; then
+                    cp $FIT_DIR/twoAndFourBody_data_split_${mode}_run${run_num}_${sign}.pdf $PLOT_DIR
+                fi
+            done
+        fi
     done
 done
 
